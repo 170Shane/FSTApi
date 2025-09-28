aa5fd6643b0dee9246cb3b3da005cde7097e8e74
Shane Clark <170shane@gmail.com>
2025-09-28 07:36:43 +0100

    Change books_post_methods to use http request Body for data rather than dictionary parameter

--- books_post_methods.py
+++ books_post_methods.py
@@ -1,15 +1,17 @@
-from fastapi import APIRouter
+from fastapi import Body,APIRouter
 import routers.books_get_methods
 
 router = APIRouter()
+
+book_list = routers.books_get_methods.books
 
 # Simple test endpoint
 # Post is the method used to create new resources on the server
 # Data is typically sent in the request body in formats like JSON
 # Example: Creating a new book entry
-@router.post("/create_book")
-async def create_book(book: dict):
-    routers.books_get_methods.books.append(book)
-    print(routers.books_get_methods.books)
-    return {"message": "Book created successfully", "book": routers.books_get_methods.books}
+@router.post("/books/create_book")
+async def create_book(book: dict = Body()):
+    book_list.append(book)
+    print(book_list)
+    return {"message": "Book created successfully", "book": book_list}
         
