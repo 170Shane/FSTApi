d8ab0744067f037018b82d0392b30951a3203a63
Shane Clark <170shane@gmail.com>
2025-10-05 07:44:01 +0100

    Correct router alias names

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from routers.books_get_methods import router as books_router
-from books_post_methods import router as put_router
+from routers.books_post_methods import router as post_router
 
 
 app = FastAPI()
 
 app.include_router(books_router)
-app.include_router(put_router)
+app.include_router(post_router)
